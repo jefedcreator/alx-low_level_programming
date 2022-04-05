ef4866eaa686f37d6a65c237a9002bb84c9f84d9
jefedcreator <jefiene77@gmail.com>
2022-04-05 08:09:06 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -41,7 +41,9 @@ for (c = height = 0; str[c] != '\0'; c++)
 if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 height++;
 aout = malloc((height + 1) * sizeof(char *))
+;
 if (aout == NULL || height == 0)
+;
 {
 free(aout);
 return (NULL);
@@ -54,7 +56,8 @@ if (str[c] == ' ')
 a1++;
 if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 {
-aout[i] = malloc((c - a1 + 2) * sizeof(char));
+aout[i] = malloc((c - a1 + 2) * sizeof(char))
+;
 if (aout[i] == NULL)
 {
 ch_free_grid(aout, i);
