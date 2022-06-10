73f92a17d205cbdd72939d3a520ff9f58519266c
jefedcreator <jefiene77@gmail.com>
2022-06-10 12:46:43 +0100

    removed whitespace

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -11,13 +11,13 @@
 
 void free_dlistint(dlistint_t *head)
 {
-  dlistint_t *tmp;
-  if (head != NULL)
-    while (head->prev != NULL)
-      head = head->prev;
-  while ((tmp = head) != NULL)
-    {
-      head = head->next;
-      free(tmp);
-    }
+dlistint_t *tmp;
+if (head != NULL)
+while (head->prev != NULL)
+head = head->prev;
+while ((tmp = head) != NULL)
+{
+head = head->next;
+free(tmp);
+}
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -12,17 +12,17 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-  int sum;
-  sum = 0;
-  if (head != NULL)
-    {
-      while (head->prev != NULL)
-	head = head->prev;
-      while (head != NULL)
-	{
-	  sum += head->n;
-	  head = head->next;
-	}
-    }
-  return (sum);
+int sum;
+sum = 0;
+if (head != NULL)
+{
+while (head->prev != NULL)
+head = head->prev;
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+}
+return (sum);
 }
